468be9b2f69acf64363de9165b816874369d840a
shinrax2 <shinra.ubu1@yahoo.de>
2020-03-12 23:58:36 +0100

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,6 @@
 @echo off
-pyinstaller -wF --noupx -n PS3GUD main.py
-copy titledb.txt dist\titledb.txt
+pyinstaller -wF --noupx -n ps3gud main.py
+mkdir dist\PS3GameUpdateDownloader
+copy titledb.txt dist\PS3GameUpdateDownloader\titledb.txt
+xcopy /E /I .\loc .\dist\PS3GameUpdateDownloader\loc
+move dist\ps3gud.exe dist\PS3GameUpdateDownloader\ps3gud.exe
