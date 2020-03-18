5a119f0f97c6db124c2ff7868ea82de66ebffd0a
shinrax2 <shinra.ubu1@yahoo.de>
2020-03-18 20:28:17 +0100

    fix build script

--- build.bat
+++ build.bat
@@ -2,5 +2,6 @@
 pyinstaller -wF --noupx -n ps3gud main.py
 mkdir dist\PS3GameUpdateDownloader
 copy titledb.txt dist\PS3GameUpdateDownloader\titledb.txt
+copy release.json dist\PS3GameUpdateDownloader\release.json
 xcopy /E /I .\loc .\dist\PS3GameUpdateDownloader\loc
 move dist\ps3gud.exe dist\PS3GameUpdateDownloader\ps3gud.exe
