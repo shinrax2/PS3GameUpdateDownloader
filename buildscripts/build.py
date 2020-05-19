18abe8792ad07f9fbc1fa50631a7930f78ca779e
shinrax2 <shinra.ubu1@yahoo.de>
2020-05-19 16:13:30 +0200

    new multiplatform build script

---
+++ buildscripts/build.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+#
+# buildscript for PS3GameUpdateDownloader by shinrax2
+
+import argparse
+import sys
+import shutil
+import os
+import platform
+import PyInstaller.__main__
+
+parser = argparse.ArgumentParser(description="buildscript for PS3GameUpdateDownloader")
+parser.add_argument("-s", action="store_true", help="building a source version")
+parser.add_argument("-c", action="store_true", help="building a compiled version")
+parser.add_argument("-r", action="store_true", help="building a release version")
+parser.add_argument("-d", action="store_true", help="building a debug version")
+args = parser.parse_args()
+#print(args)
+
+#constants
+builddir = "dist/PS3GameUpdateDownloader"
+suffix = ""
+if platform.system() == "Windows":
+    suffix = ".exe"
+
+#check parameters
+action = ""
+if args.c == True and args.s == True:
+    print("you cant pass \"-c\" and \"-s\" to the buildscript")
+    sys.exit()
+if args.d == True and args.r == True:
+    print("you cant pass \"-d\" and \"-r\" to the buildscript")
+    sys.exit()
+    
+#check cwd
+if os.path.basename(os.getcwd()) == "buildscripts":
+    print("please run the build script from the main directory")
+    sys.exit()
+
+if args.s == True and args.r == True:
+    action = "sourcerelease"
+if args.s == True and args.d == True:
+    action = "sourcedebug"
+if args.c == True and args.r == True:
+    action = "compilerelease"
+if args.c == True and args.d == True:
+    action = "compiledebug"
+
+if action == "sourcerelease":
+    if os.path.exists(builddir):
+        #delete old build
+        shutil.rmtree(builddir)
+        os.mkdir(builddir)
+    else:
+        os.mkdir(builddir)
+   #copy scripts
+    shutil.copy2("main.py", os.path.join(builddir, "main.py"))
+    shutil.copy2("utils.py", os.path.join(builddir, "utils.py"))
+    shutil.copy2("updater.py", os.path.join(builddir, "updater.py"))
+    shutil.copy2("PS3GUD.py", os.path.join(builddir, "PS3GUD.py"))
+    #copy data
+    shutil.copy2("titledb.json", os.path.join(builddir, "titledb.json"))
+    shutil.copy2("release.json", os.path.join(builddir, "release.json"))
+    shutil.copytree("./loc", os.path.join(builddir, "loc"))
+   
+if action == "sourcedebug":
+    builddir += "Debug"
+    if os.path.exists(builddir):
+        #delete old build
+        shutil.rmtree(builddir)
+        os.mkdir(builddir)
+    else:
+        os.mkdir(builddir)
+    #copy scripts
+    shutil.copy2("main.py", os.path.join(builddir, "main.py"))
+    shutil.copy2("utils.py", os.path.join(builddir, "utils.py"))
+    shutil.copy2("updater.py", os.path.join(builddir, "updater.py"))
+    shutil.copy2("PS3GUD.py", os.path.join(builddir, "PS3GUD.py"))
+    #copy data
+    shutil.copy2("titledb.json", os.path.join(builddir, "titledb.json"))
+    shutil.copy2("release.debug.json", os.path.join(builddir, "release.json"))
+    shutil.copytree("./loc", os.path.join(builddir, "loc"))
+    
+if action == "compilerelease":
+    #delete old build
+    if os.path.exists(builddir):
+        shutil.rmtree(builddir)
+        os.mkdir(builddir)
+    else:
+        os.mkdir(builddir)
+    #build main executable
+    PyInstaller.__main__.run([
+        "--name=ps3gud",
+        "--onefile",
+        "--windowed",
+        "main.py"
+    ])
+    #build updater executable
+    PyInstaller.__main__.run([
+        "--name=PS3GUDup",
+        "--onefile",
+        "--windowed",
+        "updater.py"
+    ])
+    #move executables to buildir
+    shutil.move("dist/ps3gud"+suffix, os.path.join(builddir, "ps3gud"+suffix))
+    shutil.move("dist/PS3GUDup"+suffix, os.path.join(builddir, "PS3GUDup"+suffix))
+    #copy data
+    shutil.copy2("titledb.json", os.path.join(builddir, "titledb.json"))
+    shutil.copy2("release.json", os.path.join(builddir, "release.json"))
+    shutil.copytree("./loc", os.path.join(builddir, "loc"))
+
+if action == "compiledebug":
+    builddir += "Debug"
+    #delete old build
+    if os.path.exists(builddir):
+        shutil.rmtree(builddir)
+        os.mkdir(builddir)
+    else:
+        os.mkdir(builddir)
+    #build main executable
+    PyInstaller.__main__.run([
+        "--name=ps3gud",
+        "--onefile",
+        "--windowed",
+        "main.py"
+    ])
+    #build updater executable
+    PyInstaller.__main__.run([
+        "--name=PS3GUDup",
+        "--onefile",
+        "--windowed",
+        "updater.py"
+    ])
+    #move executables to buildir
+    shutil.move("dist/ps3gud"+suffix, os.path.join(builddir, "ps3gud"+suffix))
+    shutil.move("dist/PS3GUDup"+suffix, os.path.join(builddir, "PS3GUDup"+suffix))
+    #copy data
+    shutil.copy2("titledb.json", os.path.join(builddir, "titledb.json"))
+    shutil.copy2("release.debug.json", os.path.join(builddir, "release.json"))
+    shutil.copytree("./loc", os.path.join(builddir, "loc"))
--- buildscripts/build.release.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-pyinstaller -wF --noupx -n ps3gud main.py
-pyinstaller -wF --noupx -n PS3GUDup updater.py
-mkdir dist\PS3GameUpdateDownloader
-copy titledb.txt dist\PS3GameUpdateDownloader\titledb.txt
-copy release.json dist\PS3GameUpdateDownloader\release.json
-xcopy /E /I /Y .\loc .\dist\PS3GameUpdateDownloader\loc
-move dist\ps3gud.exe dist\PS3GameUpdateDownloader\ps3gud.exe
-move dist\PS3GUDup.exe dist\PS3GameUpdateDownloader\PS3GUDup.exe
