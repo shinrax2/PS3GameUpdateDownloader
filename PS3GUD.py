8e17e34420b760514154e7c8454484e31d506edc
shinrax2 <shinra.ubu1@yahoo.de>
2020-07-07 15:39:46 +0200

    fix crash when downloading on python 3.8

--- PS3GUD.py
+++ PS3GUD.py
@@ -205,7 +205,7 @@ class PS3GUD():
             if free > int(size):
                 with requests.get(url, stream=True) as r:
                     r.raise_for_status()
-                    start = time.clock()
+                    start = time.perf_counter()
                     with open(local_filename, 'wb') as f:
                         for chunk in r.iter_content(chunk_size=chunk_size): 
                             if chunk:
@@ -215,7 +215,7 @@ class PS3GUD():
                                 if already_loaded / size > 1:
                                     already_loaded = size
                                 percentage = already_loaded / size * 100
-                                text.Update(self.loc.getKey("window_main_progress_label", [num, utils.formatSize(already_loaded), utils.formatSize(size), format(float(percentage), '.1f'), utils.formatSize(already_loaded//(time.clock() - start))+"s"]))
+                                text.Update(self.loc.getKey("window_main_progress_label", [num, utils.formatSize(already_loaded), utils.formatSize(size), format(float(percentage), '.1f'), utils.formatSize(already_loaded//(time.perf_counter() - start))+"s"]))
                                 bar.UpdateBar(percentage)
                                 window.Refresh()
             else:
